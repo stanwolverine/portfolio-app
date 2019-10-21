6920bd2206747bf37749e201d26b253b8a4daa5b
Jatin Nagar <jitu19nagar@gmail.com>
2019-10-21 07:47:39 +0530

    added react fragments

--- pages/about.js
+++ pages/about.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react';
 import Link from 'next/link';
+import Header from '../components/Header';
 
 export default class contact extends Component {
-  render() {
-    return (
-      <div>
-        This is about page.
-        <Link href='/'>
-          <a>
-            Take me to <span>Home</span> page
-          </a>
-        </Link>
-        <Link href='/contact'>
-          <a>
-            Take me to <span>Contact</span> page
-          </a>
-        </Link>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<>
+				<Header />
+				<div>
+					This is about page.
+					<Link href='/'>
+						<a>
+							Take me to <span>Home</span> page
+						</a>
+					</Link>
+					<Link href='/contact'>
+						<a>
+							Take me to <span>Contact</span> page
+						</a>
+					</Link>
+				</div>
+			</>
+		);
+	}
 }
--- pages/contact.js
+++ pages/contact.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react';
 import Link from 'next/link';
+import Header from '../components/Header';
 
 export default class contact extends Component {
-  render() {
-    return (
-      <div>
-        This is contact page.
-        <Link href='/'>
-          <a>
-            Take me to <span>Home</span> page
-          </a>
-        </Link>
-        <Link href='/about'>
-          <a>
-            Take me to <span>About</span> page
-          </a>
-        </Link>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<>
+				<Header />
+				<div>
+					This is contact page.
+					<Link href='/'>
+						<a>
+							Take me to <span>Home</span> page
+						</a>
+					</Link>
+					<Link href='/about'>
+						<a>
+							Take me to <span>About</span> page
+						</a>
+					</Link>
+				</div>
+			</>
+		);
+	}
 }
