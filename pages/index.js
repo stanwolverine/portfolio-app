2953ca50713e223e35744ed8aad841ff6c09b3af
Jatin Nagar <jitu19nagar@gmail.com>
2019-10-23 08:31:36 +0530

    added new section

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Link from 'next/link';
 import Header from '../components/Header';
+import HeroSection from '../components/HeroSection';
 
 const Home = () => (
 	<div>
 		<Header />
+		<HeroSection />
 	</div>
 );
 
